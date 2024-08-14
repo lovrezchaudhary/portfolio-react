49a0c96f99275ab1750310218599e889d86c14aa
lovrezchaudhary <lovrezchaudhary@gmail.com>
2024-08-15 00:54:00 +0530

    Add Navbar
    Design fully responsive navbar for all display.

--- my-portfolio/portfolio/src/App.jsx
+++ my-portfolio/portfolio/src/App.jsx
@@ -1,12 +1,13 @@
-
+import Navbar from './Components/Navbar/Navbar'
 
 function App() {
  
 
   return (
-    <>
-      
-    </>
+    <div className="bg-[#171d32] h-auto w-full overflow-hidden">
+      <Navbar />
+
+    </div>
   )
 }
 
