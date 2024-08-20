799dcb5b8c53dc1ccc0c455faacdb9b3cf1aea2b
lovrezchaudhary <lovrezchaudhary@gmail.com>
2024-08-20 23:30:25 +0530

    Design Experience Section

--- my-portfolio/portfolio/src/App.jsx
+++ my-portfolio/portfolio/src/App.jsx
@@ -12,7 +12,7 @@ function App() {
       <Home />
       <About />
       <Experience />
-
+      
     </div>
   )
 }
--- my-portfolio/portfolio/src/Components/Experience/Experience.jsx
+++ my-portfolio/portfolio/src/Components/Experience/Experience.jsx
@@ -1,7 +1,6 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
-import { FaCss3, FaFigma, FaHtml5, FaJs } from 'react-icons/fa'
-import { SiRadis } from 'react-icons/si'
+import { FaCss3, FaFigma, FaHtml5, FaJs, FaReact } from 'react-icons/fa'
 import { faGoogle } from 'react-icons/fa'
 import { SiMongodb } from 'react-icons/si'
 import { RiNetflixFill } from '@remixicon/react'
@@ -9,8 +8,66 @@ import { FaAmazon } from 'react-icons/fa'
 
 const Experience = () => {
   return (
-    <div>
-      
+    <div id="Experience" className="p-10 md:p-24">
+      <h1 className="text-2xl md:text-4xl text-white font-bold">Experience</h1>
+      <div className="flex flex-wrap items-center justify-around">
+        <div className="flex flex-wrap md:w-2/5 gap-8 md:p-12 py-10">
+            <span className="p-3 bg-zinc-950 flex items-center rounded-2xl">
+                <FaHtml5 color="#E34F26" size={50} />
+            </span>
+            <span className="p-3 bg-zinc-950 flex items-center rounded-2xl">
+                <FaCss3 color="#1572b6" size={50} />
+            </span>
+            <span className="p-3 bg-zinc-950 flex items-center rounded-2xl">
+                <FaReact color="#61DAFB" size={50} />
+            </span>
+            <span className="p-3 bg-zinc-950 flex items-center rounded-2xl">
+                <FaJs color="#F7Df1E" size={50} />
+            </span>
+            <span className="p-3 bg-zinc-950 flex items-center rounded-2xl">
+                <FaFigma color="#F24E1E" size={50} />
+            </span>
+            <span className="p-3 bg-zinc-950 flex items-center rounded-2xl">
+                <SiMongodb color="#47A248" size={50} />
+            </span>
+
+        </div>
+        <div>
+        <div className="flex gap-10 bg-slate-950 bg-opacity-45 mt-4 rounded-lg p-4 items-center">
+            <faGoogle color="#4285F4" size={50} />
+            <span className="text-white">
+                <h2 className="leading-tight ">Software Engineer, Google</h2>
+                <p className="text-sm leading-tight font-thin">Sept-2023 - Present</p>
+                <ul className="text-sm p-2">
+                    <li>- Work as Software Developer</li>
+                    <li>- Senior SDE Devloper</li>
+                </ul>
+            </span>
+        </div>
+        <div className="flex gap-10 bg-slate-950 bg-opacity-45 mt-4 rounded-lg p-4 items-center">
+            <RiNetflixFill color="#E50914" size={50} />
+            <span className="text-white">
+                <h2 className="leading-tight ">Software Engineer, Netflix</h2>
+                <p className="text-sm leading-tight font-thin">Sept-2023 - Present</p>
+                <ul className="text-sm p-2">
+                    <li>- Work as Software Developer</li>
+                    <li>- Senior SDE Devloper</li>
+                </ul>
+            </span>
+        </div>
+        <div className="flex gap-10 bg-slate-950 bg-opacity-45 mt-4 rounded-lg p-4 items-center">
+            <FaAmazon color="#FF9900" size={50} />
+            <span className="text-white">
+                <h2 className="leading-tight ">Software Engineer, Amazon</h2>
+                <p className="text-sm leading-tight font-thin">Sept-2023 - Present</p>
+                <ul className="text-sm p-2">
+                    <li>- Work as Software Developer</li>
+                    <li>- Senior SDE Devloper</li>
+                </ul>
+            </span>
+        </div>
+        </div>
+      </div>
     </div>
   )
 }
