2a238d7b5b8a0c0da5170289f1e6d0cd877f0d66
lovrezchaudhary <lovrezchaudhary@gmail.com>
2024-08-21 23:10:12 +0530

    Create Project.jsx

---
+++ my-portfolio/portfolio/src/Components/Project/Project.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+const Project = () => {
+  return (
+    <div id="Project" className="p-10 md:p-24 text-white">
+      
+    </div>
+  )
+}
+
+export default Project
